23fa0769fa21a216aeb338fdde259f567a5714c0
Thomas William Page <tom.w.page@gmail.com>
2017-03-13 23:42:25 +0100

    first commit

---
+++ hello-world/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HelloWorld from './HelloWorld';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <HelloWorld name='BlaapBloops'/>
+      <HelloWorld name='FreemsFrooops'/>
+    </div>
+  );
+};
+
+export default App;
---
+++ hello-world/src/HelloWorld.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './HelloWorld.css';
+
+class HelloWorld extends Component{
+  constructor(props) {
+    super(props);
+    this.state = { greeting: 'Hello',
+                  phrase: 'this is major Tom.'
+                };
+  }
+
+  frenchify() {
+    this.setState({ greeting: 'Bonjour' });
+  }
+
+  render() {
+    return (
+      <div className="HelloWorld">
+        {this.state.greeting} {this.props.name} {this.state.phrase}!
+        <br/>
+        <button onClick={this.frenchify}>Frenchify</button>
+      </div>
+    );
+  }
+};
+
+
+export default HelloWorld;
